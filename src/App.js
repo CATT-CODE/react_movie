1a2f8bbb5ad07c58e17feb394e2b5ac761169b45
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2021-03-10 23:54:23 -0500

    done-ish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class App extends Component {
+  
+  state = {
+    isLoading: true,
+    movieArray: [],
+    inputMovie: "",
+    errorMessage: "",
+  };
+
+  handleOnChange = (event) => {
+    this.setState({
+      inputMovie: event.target.value,
+      errorMessage: "",
+    });
+  }
+
+  handleOnSubmit = async (event) => {
+    if (this.state.inputMovie.length === 0) {
+      this.setState({
+        errorMessage: "Please provide a valid input",
+      })
+    } else {
+      let result = await axios.get(
+        `http://www.omdbapi.com/?s=${this.state.inputMovie}&apikey=96fac7a1`
+      );
+      this.setState({
+        movieArray: result.data.Search
+      });
+    };
+    };
+  
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Enter Movie Title"
+          name="inputMovie"
+          onChange={this.handleOnChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </input>
+        <button
+          onClick={this.handleOnSubmit}
+          type="submit"
+        >
+          Submit
+        </button>
+        <br/>
+        <div className="row">
+          {this.state.errorMessage.length > 0 ? <div>{this.state.errorMessage}</div> : <div>{this.state.movieArray.map((movie) => <div key={movie.id}>{movie.Title}</div>)}</div>}
+          
+          
+      </div>
+      </div>
+    )
+  }
 }
-
-export default App;
